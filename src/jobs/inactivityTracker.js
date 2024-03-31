2b6abbb7b40d3b8a3ad06a3f5b969f851c60e8f8
D G Shivu <dgshivu3232@gmail.com>
2024-03-31 12:12:12 +0530

    init: inactivityTracker worker & job

---
+++ src/jobs/inactivityTracker.js
@@ -0,0 +1,22 @@
+const cron = require("node-cron");
+const logger = require("../config/logger");
+const {
+  userInactivityTracker,
+} = require("../workers/userInactivityTracker/index");
+
+const userInactivityTrackerJob = cron.schedule(
+  "59 23 * * *",
+  () => {
+    logger.info("Running user inactivity tracker cron job...");
+    userInactivityTracker();
+  },
+  {
+    timezone: "Asia/Kolkata",
+  }
+);
+
+userInactivityTrackerJob.on("error", (err) => {
+  logger.error("Error in user inactivity tracker cron job:", err);
+});
+
+module.exports = userInactivityTrackerJob;
