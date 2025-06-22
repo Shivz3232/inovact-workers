3a5197eca7b2177958e97d8561b5277777d47c0c
Pratik Sunil Pendurkar <pendurkarpratik04@gmail.com>
2025-06-22 16:48:57 +0530

    chore: add status filter to recruitment jobs query for deadline reminders

--- src/workers/applications.worker/oneDayToDeadlineReminder/queries/queries.js
+++ src/workers/applications.worker/oneDayToDeadlineReminder/queries/queries.js
@@ -1,5 +1,8 @@
 const fetchJobsWithinDeadlineQuery = `query FetchJobsWithinDeadline($timestamp: timestamp) {
-  recruitment_jobs(where: {deadline: {_lte: $timestamp}, applications_aggregate: {count: {predicate: {_neq: 0}}}}) {
+  recruitment_jobs(where: {
+    deadline: {_lte: $timestamp}, 
+    status: {_neq: "closed"},
+    applications_aggregate: {count: {predicate: {_neq: 0}}}}) {
     id
     applications {
       applicant {
